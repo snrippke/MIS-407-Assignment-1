6c0d07ccd6fae611e6151a9aaac2dae6d7d1580b
snrippke <snrippke@iastate.edu>
2015-09-16 23:33:41 -0500

    Update myCSV3.py

--- myCSV3.py
+++ myCSV3.py
@@ -1,16 +1,17 @@
 import csv
 
-with open('myData.csv') as csvfile:
+with open('C:/Python34/myPy/myData.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     dates = []
     colors = []
-#   prices = []
+    prices = []
     for row in readCSV:
         color = row[3]
         date = row[0]
-#       price = row[4]
+        price = row[4]
         dates.append(date)
         colors.append(color)
+        prices.append(price)
 
     print(dates)
     print(colors)
@@ -22,4 +23,8 @@ with open('myData.csv') as csvfile:
     theDate = dates[coldex]
     print('The date of',whatColor,'is:',theDate)
     
-#   print the price of the Yellow shirt    
+#   print the price of the Yellow shirt
+    whatColorPrice = input('What color do you wish to know the price of?: ')
+    coldexPrice = colors.index(whatColorPrice)
+    thePrice = prices[coldexPrice]
+    print('The price of',whatColorPrice,'is:',thePrice)
